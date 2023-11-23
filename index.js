28a6ada048f7f93b84711753470ab96b97e4c004
hossainta1 <tanvir.hossain303030@gmail.com>
2023-11-23 20:28:46 +0600

    Log in with access token

--- index.js
+++ index.js
@@ -26,6 +26,23 @@ const client = new MongoClient(uri, {
     }
 });
 
+const verifyJwt = (req, res, next) => {
+    const authorization = req.headers.authorization;
+    if (!authorization) {
+        return res.status(401).send({ error: true, message: 'unauthorized access' });
+    }
+    const token = authorization.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(403).send({ error: true, message: 'unauthorized access' });
+        }
+        req.decoded = decoded;
+        next();
+    })
+
+}
+
+
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
@@ -41,7 +58,7 @@ async function run() {
             const user = req.body;
             console.log(user);
 
-            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1hr' });
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
             console.log(token);
             res.send({ token });
         })
@@ -72,8 +89,17 @@ async function run() {
 
         // Booking  section
 
-        app.get('/bookings', async (req, res) => {
-            console.log(req.query.email)
+        app.get('/bookings', verifyJwt, async (req, res) => {
+            const decoded = req.decoded;
+            console.log('Come back after verify', decoded);
+
+            if (decoded.email !== req.query.email) {
+
+                return res.status(403).send({ error: 1, message: 'Forviden Access denied' })
+
+            }
+
+
             let query = {};
             if (req.query?.email) {
                 query = { email: req.query.email }
